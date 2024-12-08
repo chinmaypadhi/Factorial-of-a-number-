c8c8ffbc6a219afe7309f9bedaff64c653c65ee8
chinmay padhi <padhichinmay108@gmail.com>
2024-12-08 12:20:31 +0530

    first commit

---
+++ Factorial of a no/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Factorial_of_a_no
+{
+    class Program
+    {
+
+        static async void  factorial(int no)
+        {
+        Task.Run(() =>
+              {
+
+                  int fact = 1;
+
+                  for (int i = 1; i <= no; i++)
+                  {
+                      fact = fact * i;
+                  }
+
+                  Console.WriteLine(fact);
+              });
+        }
+        static void Main(string[] args)
+        {
+            factorial(3);
+            Console.ReadLine();
+        }
+    }
+}
